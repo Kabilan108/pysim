8f1b2a5792f2a6a0e6632405c0b90ebe2d7fadc1
Tony Okeke <58089381+Kabilan108@users.noreply.github.com>
2023-01-27 01:11:45 -0500

    Add some set up code

---
+++ pysim/__init__.py
@@ -0,0 +1,12 @@
+"""
+pysim - Python Wrapper for Simulink
+"""
+
+# Imports from standard library
+from importlib.metadata import version
+
+# Package metadata
+__version__ = '0.0.1'
+
+# Populate package namespace
+from pysim.pysim import Simulink, get_knee_jerk_model
